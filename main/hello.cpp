5c722cb784987603c0dc4d43caae3bf41add52de
Seetharam U N <unseetharam@gmail.com>
2023-02-17 20:08:45 +0530

    This is my commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World This is Seetharam\n";
+  cout << "Hello, Jenkins My SRN is PES2UG20CS318\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
